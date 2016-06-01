e62fcfff0a0c23271a805004ebae1d0eb3e64137
Santiago Baldrich <santiago.baldrich@gmail.com>
2016-06-01 04:54:22 -0500

    FP in Scala: ch06 Incomplete

--- scala/fpinscala/ch06-purelyfuncstate.scala
+++ scala/fpinscala/ch06-purelyfuncstate.scala
@@ -1,8 +1,9 @@
-trait RNG{
-  def nextInt : (Int, RNG)
+
+trait RNG {
+  def nextInt: (Int, RNG)
 }
 
-object RNG{
+object RNG {
 
   case class Simple(seed: Long) extends RNG {
 
@@ -12,57 +13,111 @@ object RNG{
       val n = (newSeed >>> 16).toInt
       (n, nextRNG)
     }
-    //6.1
-    def nonNegativeInt(rng : RNG) : (Int, RNG) = {
-      val (i, r) = rng.nextInt
-      (if (i < 0) -(i + 1) else i, r)
-    }
-    //6.2
-    def double(rng : RNG) : (Double, RNG) = {
-      val (i, r) = nonNegativeInt(rng)
-      (i / Int.MaxValue.toDouble + 1, r)
-    }
-
-    //6.3
-    def intDouble(rng : RNG) : ((Int, Double), RNG) ={
-      val (i, r) = nextInt
-      val (d, r2) = double(r)
-      ((i,d), r2)
-    }
-
-    def doubleInt(rng : RNG) : ((Double, Int), RNG) = {
-      val ((i,d), r) = intDouble(rng)
-      ((d, i), r)
-    }
-
-    def double3(rng : RNG) : ((Double, Double, Double), RNG) = {
-      val (d1, r) = double(rng)
-      val (d2, r1) = double(r)
-      val (d3, r2) = double(r1)
-      ((d1,d2,d3), r2)
-    }
-    //6.4
-    def ints(count : Int)(rng : RNG) : (List[Int], RNG) =
-      if(count == 0)
-        (List(), rng)
-      else{
-        val(i, r) = nextInt
-        val(xs, r1) = ints(count - 1)(r)
-        ((i :: xs), r1)
-      }
-
-    type Rand[+A] = RNG => (A, RNG)
-
-    val int : RNG => (Int, RNG) = _.nextInt
-
-    def map[A,B](s : Rand[A])(f : A => B) : Rand[B] =
-      rng => {
-        val (a, rng2) = s(rng)
-        (f(a),rng2)
-      }
-
-    def nonNegativeEven : Rand[Int] =
-      map(nonNegativeInt)(i => i - i % 2)
-
   }
+
+  //6.1
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+  //6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / Int.MaxValue.toDouble + 1, r)
+  }
+
+  //6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r) = double(rng)
+    val (d2, r1) = double(r)
+    val (d3, r2) = double(r1)
+    ((d1, d2, d3), r2)
+  }
+
+  //6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    if (count == 0)
+      (List(), rng)
+    else {
+      val (i, r) = rng.nextInt
+      val (xs, r1) = ints(count - 1)(r)
+      ((i :: xs), r1)
+    }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: RNG => (Int, RNG) = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  //6.5
+  def double2: Rand[Double] =
+    map(nonNegativeInt)(_ / Int.MaxValue.toDouble + 1)
+
+  //6.6
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r) = ra(rng)
+      val (b, r1) = rb(r)
+      f(a, b) -> r1
+    }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  val randIntDouble = both(int, double)
+  val randDoubleInt = both(double, int)
+
+  //6.7
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+  def ints2(count : Int) : Rand[List[Int]] =
+    sequence(List.fill(count)(int))
+
+  //6.8 (I still don't get this one)
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+  }
+
+  //6.9 (I still don't get this one)
+  def _map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+
+  //6.10 - 6.11 I'll have to return to this chapter
+
 }
